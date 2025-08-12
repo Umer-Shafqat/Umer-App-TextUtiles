f86827e6ed4159c7503bca85043b0b26e42a0f13
M.Umer Shafqat <umershafqat251786@gmail.com>
2025-08-12 13:10:07 +0500

    Add Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
 import './App.css';
-
+import NAVBAR from './components/NAVBAR';
 function App() {
   return (
     <>
- 
-
-  </>
+      <NAVBAR title="Textutils" aboutText="About Textutils"/>
+    </>
   );
 }
-
 export default App;
