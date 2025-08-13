05aa2e57456cc62b2fc3aafc0632679956b94c82
M.Umer Shafqat <umershafqat251786@gmail.com>
2025-08-13 14:58:51 +0500

    Add About.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import NAVBAR from './components/NAVBAR';
 import TextForm from './components/TextForm';
+import About from './components/About'; 
 
 function App() {
   return (
     <>
       <NAVBAR title="Textutils" aboutText="About Textutils"/>
+      <About />
       <div className="container">
         <TextForm heading="Enter Text Below"/>
       </div>
