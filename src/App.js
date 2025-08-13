fec37fc27a0491bad8de7cd89952ced4c7b0cfa3
M.Umer Shafqat <umershafqat251786@gmail.com>
2025-08-13 13:17:51 +0500

    add TextForm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
 import NAVBAR from './components/NAVBAR';
+import TextForm from './components/TextForm';
+
 function App() {
   return (
     <>
       <NAVBAR title="Textutils" aboutText="About Textutils"/>
+      <div className="container">
+        <TextForm heading="Enter Text Below"/>
+      </div>
     </>
   );
 }
 export default App;
+
