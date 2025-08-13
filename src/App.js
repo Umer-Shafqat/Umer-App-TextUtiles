2b03e3848e932d1f196eefb577cb532c51efb0ae
M.Umer Shafqat <umershafqat251786@gmail.com>
2025-08-13 20:25:12 +0500

    add Alert

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
+import Alert from './components/alert';
 import NAVBAR from './components/NAVBAR';
 import TextForm from './components/TextForm';
 //import About from './components/About'; 
 import React,{useState} from 'react';
+
 
 function App() {
   const [Mode,setMode] = useState("light");
@@ -22,6 +24,7 @@ function App() {
   return (
     <>
       <NAVBAR title="Textutils" aboutText="About Textutils" mode={Mode}  togglemode={toggleMode}/>
+      <Alert alert="This is an alert message!" />
       {/* <About /> */}
       <div className="container">
         <TextForm heading="Enter Text Below" mode={Mode} />
