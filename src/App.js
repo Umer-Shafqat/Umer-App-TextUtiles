243faa036565b46a92001366b67513d23c3c2644
M.Umer Shafqat <umershafqat251786@gmail.com>
2025-08-13 18:34:49 +0500

    add functionallity in navbar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import './App.css';
 import NAVBAR from './components/NAVBAR';
 import TextForm from './components/TextForm';
 //import About from './components/About'; 
+import React,{useState} from 'react';
 
 function App() {
+  const [Mode,setMode] = useState("light");
+
+  const toggleMode = () => {
+    if (Mode === "light") {
+      setMode("dark");
+    } else {
+      setMode("light");
+    }
+  };
   return (
     <>
-      <NAVBAR title="Textutils" aboutText="About Textutils"/>
+      <NAVBAR title="Textutils" aboutText="About Textutils" mode={Mode} togglemode={toggleMode}/>
       {/* <About /> */}
-      { <div className="container">
+      <div className="container">
         <TextForm heading="Enter Text Below"/>
-  </div>}
+  </div>
     </>
   );
 }
