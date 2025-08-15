847be862dd8b0edff996a7b07df470af44257dbb
M.Umer Shafqat <umershafqat251786@gmail.com>
2025-08-15 20:20:02 +0500

    add files

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import Alert from './components/Alert';
 import NAVBAR from './components/NAVBAR';
 import TextForm from './components/TextForm';
-//import About from './components/About';
+import About from './components/About';
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [Mode, setMode] = useState("light");
@@ -31,14 +32,15 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <NAVBAR title="Textutils" aboutText="About Textutils" mode={Mode} togglemode={toggleMode} />
       <Alert alert={alert} />
 
-      {/* Choose which component to display here */}
-      <TextForm heading="Enter Text Below" mode={Mode} showAlert={showAlert} />
-      {/* <About /> */}
-    </>
+      <Routes>
+        <Route path="/" element={<TextForm heading="Enter Text Below" mode={Mode} showAlert={showAlert} />} />
+        <Route path="/about" element={<About mode={Mode} />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -39,17 +39,17 @@ const handleextraspace = () => {
       <div className = "mb-3" >
         <textarea className="form-control" value={text} onChange={handleOnChange} style={{background : prop.mode === "light" ? 'wheat' : 'grey'}} id="mybox" rows="8"></textarea>
       </div>
-      <button style={{color:prop.mode === "dark" ? 'grey' : 'black'}} className="btn btn-light mx-2 " onClick={handleUpClick}>Convert to UpperCase</button>
-      <button  style={{color:prop.mode === "dark" ? 'grey' : 'black'}} className="btn btn-light mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
-      <button style={{color:prop.mode === "dark" ? 'grey' : 'black'}} className="btn btn-light mx-2" onClick={handlefirstclick}>Convert to First Capital</button>
-      <button style={{color:prop.mode === "dark" ? 'grey' : 'black'}} className="btn btn-light mx-2" onClick={handleextraspace}>Remove extra Spaces</button>
-      <button style={{color:prop.mode === "dark" ? 'grey' : 'black'}} className="btn btn-light mx-2" onClick={clear}>Clear</button>
+      <button disabled={text.length===0} style={{color:prop.mode === "dark" ? 'grey' : 'black'}} className="btn btn-light mx-2 my-2" onClick={handleUpClick}>Convert to UpperCase</button>
+      <button  disabled={text.length===0} style={{color:prop.mode === "dark" ? 'grey' : 'black'}} className="btn btn-light mx-2 my-2" onClick={handleLowClick}>Convert to LowerCase</button>
+      <button disabled={text.length===0} style={{color:prop.mode === "dark" ? 'grey' : 'black'}} className="btn btn-light mx-2 my-2" onClick={handlefirstclick}>Convert to First Capital</button>
+      <button disabled={text.length===0} style={{color:prop.mode === "dark" ? 'grey' : 'black'}} className="btn btn-light mx-2 my-2" onClick={handleextraspace}>Remove extra Spaces</button>
+      <button disabled={text.length===0} style={{color:prop.mode === "dark" ? 'grey' : 'black'}} className="btn btn-light mx-2 my-2" onClick={clear}>Clear</button>
     </div>
 
     <div className="container my-3" style={{color:prop.mode === "dark" ? 'wheat' : 'grey'}}>
      <h2>Your Text Summary</h2>
-     <p>{text.split(" ").length-1} words and {text.length} characters </p>
-     <p>{0.008 * (text.split(" ").length-1)} minutes read</p>
+     <p>{text.split(" ").filter((element) => { return element.length !== 0 }).length} words and {text.length} characters </p>
+     <p>{0.008 * (text.split(" ").filter((element) => { return element.length !== 0 }).length)} minutes read</p>
      <h2>Preview</h2>
      <p>{text.length>0 ? text : "Nothing to preview!"}</p>
     </div>
